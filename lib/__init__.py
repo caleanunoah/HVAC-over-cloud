8227e45550d3c99d6d02916a344e94f989f0d767
caleanunoah <43590970+caleanunoah@users.noreply.github.com>
2023-05-05 11:48:59 -0700

    read all 4 NEU sensors

--- lib/__init__.py
+++ lib/__init__.py
@@ -1,5 +1,5 @@
 local_ipv4_addr = "192.168.1.70/24"                      # IPv4 Address - local machine's Wireless LAN
-honeywell_301C_ipv4_addr = "192.168.1.72"               # IPv4 Address - Honeywell 301C Controller
+honeywell_301C_ipv4_addr = "192.168.1.70"               # IPv4 Address - Honeywell 301C Controller
 ethernet_ipv4_addr = "192.168.1.80/24"                   # IPv4 Address - Ethernet switch
 
 ipv4_pc = "192.168.1.70/24"
@@ -9,5 +9,7 @@ ipv4_orangepi = "192.168.1.75/24"
 BAC0_port = "47808"                                      # This is a defined in BACnet as the port #
 
 # Sensor IDs (Founding YABE)
-OBJ_ID_PPM = ["513"]                                       # BACnet ID of PPM reading found using (YABE: https://sourceforge.net/projects/yetanotherbacnetexplorer/)
+# NEU RLDS IDs
+#             [Adr 2, Adr. 1, Adr 3, Adr 4]
+OBJ_ID_PPM = ["513", "257", "769", "1025"]                                       # BACnet ID of PPM reading found using (YABE: https://sourceforge.net/projects/yetanotherbacnetexplorer/)
 OBJ_ID_BUZZ = ["46360"]                                    # BACnet ID of PPM reading found using (YABE: https://sourceforge.net/projects/yetanotherbacnetexplorer/)
--- main.py
+++ main.py
@@ -47,45 +47,53 @@ def animate(i, xs, ys):
     plt.subplots_adjust(bottom=0.30)
 
 
-def get_data(bacnet_cnx):
+def get_data(bacnet_cnx, device_ID):
     dt = datetime.datetime.now(pytz.timezone('Canada/Pacific')).isoformat()
     return {
         'event_time': datetime.datetime.now().isoformat(),
-        'rlds_ID': "rid_123",
-        'sensor_ID': 'sid_1',
-        'ppm': bacnet_cnx.read(lib.honeywell_301C_ipv4_addr + ":" + lib.BAC0_port + " analogInput " + lib.OBJ_ID_PPM[0] + " presentValue"),
-        'ppm_status_flags': bacnet_cnx.read(lib.honeywell_301C_ipv4_addr + ":" + lib.BAC0_port + " analogInput " + lib.OBJ_ID_PPM[0] + " statusFlags")
+        'rlds_ID': "NEU",
+        'sensor_ID': bacnet_cnx.read(lib.honeywell_301C_ipv4_addr + ":" + lib.BAC0_port + " analogInput " + device_ID + " objectIdentifier"),
+        'sensor_name': bacnet_cnx.read(lib.honeywell_301C_ipv4_addr + ":" + lib.BAC0_port + " analogInput " + device_ID + " objectName"),
+        'ppm': bacnet_cnx.read(lib.honeywell_301C_ipv4_addr + ":" + lib.BAC0_port + " analogInput " + device_ID + " presentValue"),
+        'ppm_status_flags': bacnet_cnx.read(lib.honeywell_301C_ipv4_addr + ":" + lib.BAC0_port + " analogInput " + device_ID + " reliability"),
+        'out_of_service': bacnet_cnx.read(lib.honeywell_301C_ipv4_addr + ":" + lib.BAC0_port + " analogInput " + device_ID + " outOfService"),
+
     }
 
 def generate(stream_name, kinesis_client, bacnet_cnx):
     while True:
-        data = get_data(bacnet_cnx)
-        print(data)
+        for id in lib.OBJ_ID_PPM:
+            data = get_data(bacnet_cnx, id)
+            print(data)
+
+            kinesis_client.put_record(
+                StreamName=stream_name,
+                Data=json.dumps(data),
+                PartitionKey='partitionkey')
 
         time.sleep(60)
 
-        kinesis_client.put_record(
-            StreamName=stream_name,
-            Data=json.dumps(data),
-            PartitionKey='partitionkey')
 
 
 if __name__ == '__main__':
 
-    bacnet = BAC0.lite(ip=lib.ipv4_orangepi, port="47808")
+    bacnet = BAC0.lite(ip=lib.ethernet_ipv4_addr, port="47808")
     bacnet.whois()  # Prints 301C's IPv4 192.168.1.72
     print(bacnet.devices)
 
+    time.sleep(1)
+    #controller = BAC0.device(address=lib.honeywell_301C_ipv4_addr, device_id=1, network=bacnet)
+    #print(controller.points)
     #ani = animation.FuncAnimation(fig, animate, fargs=(xs, ys), interval=3000)
     #plt.show()
 
     # ONLINE
-    '''
+
     generate(stream_name="OrangePi",
              kinesis_client=boto3.client('kinesis', region_name='us-west-2'),
              bacnet_cnx=bacnet)
-    '''
 
+    '''
     # OFFLINE
     while True:
         local_data = json.dumps(get_data(bacnet), indent=2)
@@ -100,7 +108,7 @@ if __name__ == '__main__':
         f.close()
 
         time.sleep(60)
-
+'''
 
 
 
